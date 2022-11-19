cc41cbc0572debe3aaa7e8f0215acfe0c021749f
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-11-19 20:17:45 +0700

    a

--- funny.sh
+++ funny.sh
@@ -949,6 +949,7 @@ systemctl restart ws-dropbear.service
 clear
 
 #banner
+cd
 echo "================  Banner ======================"
 wget -O /etc/issue.net "https://gitlab.com/hidessh/baru/-/raw/main/banner.conf"
 chmod +x /etc/issue.net
@@ -957,6 +958,86 @@ echo "Banner /etc/issue.net" >> /etc/ssh/sshd_config
 echo "DROPBEAR_BANNER="/etc/issue.net"" >> /etc/default/dropbear
 
 
+
+# download script
+cd /usr/bin
+wget -O menu-ssh "https://raw.githubusercontent.com/bracoli/v4/main/menu/menu-ssh.sh"
+wget -O usernew "https://raw.githubusercontent.com/bracoli/v4/main/ssh/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/bracoli/v4/main/ssh/trial.sh"
+wget -O renew "https://raw.githubusercontent.com/bracoli/v4/main/ssh/renew.sh"
+wget -O hapus "https://raw.githubusercontent.com/bracoli/v4/main/ssh/hapus.sh"
+wget -O cek "https://raw.githubusercontent.com/bracoli/v4/main/ssh/cek.sh"
+wget -O member "https://raw.githubusercontent.com/bracoli/v4/main/ssh/member.sh"
+wget -O delete "https://raw.githubusercontent.com/bracoli/v4/main/ssh/delete.sh"
+wget -O autokill "https://raw.githubusercontent.com/bracoli/v4/main/ssh/autokill.sh"
+wget -O ceklim "https://raw.githubusercontent.com/bracoli/v4/main/ssh/ceklim.sh"
+wget -O tendang "https://raw.githubusercontent.com/bracoli/v4/main/ssh/tendang.sh"
+
+# menu system
+wget -O menu-set "https://raw.githubusercontent.com/bracoli/v4/main/menu/menu-set.sh"
+wget -O menu-domain "https://raw.githubusercontent.com/bracoli/v4/main/menu/menu-domain.sh"
+wget -O add-host "https://raw.githubusercontent.com/bracoli/v4/main/ssh/add-host.sh"
+wget -O port-change "https://raw.githubusercontent.com/bracoli/v4/main/port/port-change.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/bracoli/v4/main/xray/certv2ray.sh"
+wget -O menu-webmin "https://raw.githubusercontent.com/bracoli/v4/main/menu/menu-webmin.sh"
+wget -O speedtest "https://raw.githubusercontent.com/bracoli/v4/main/ssh/speedtest_cli.py"
+wget -O about "https://raw.githubusercontent.com/bracoli/v4/main/menu/about.sh"
+wget -O auto-reboot "https://raw.githubusercontent.com/bracoli/v4/main/menu/auto-reboot.sh"
+wget -O restart "https://raw.githubusercontent.com/bracoli/v4/main/menu/restart.sh"
+wget -O bw "https://raw.githubusercontent.com/bracoli/v4/main/menu/bw.sh"
+
+# change port
+wget -O port-ssl "https://raw.githubusercontent.com/bracoli/v4/main/port/port-ssl.sh"
+wget -O port-ovpn "https://raw.githubusercontent.com/bracoli/v4/main/port/port-ovpn.sh"
+
+
+wget -O xp "https://raw.githubusercontent.com/bracoli/v4/main/ssh/xp.sh"
+wget -O acs-set "https://raw.githubusercontent.com/bracoli/v4/main/acs-set.sh"
+wget -O userdelexpired "https://gitlab.com/hidessh/baru/-/raw/main/userdelexpired.sh"
+
+
+chmod +x userdelexpired
+chmod +x menu-ssh
+chmod +x usernew
+chmod +x trial
+chmod +x renew
+chmod +x hapus
+chmod +x cek
+chmod +x member
+chmod +x delete
+chmod +x autokill
+chmod +x ceklim
+chmod +x tendang
+
+chmod +x menu-set
+chmod +x menu-domain
+chmod +x add-host
+chmod +x port-change
+chmod +x certv2ray
+chmod +x menu-webmin
+chmod +x speedtest
+chmod +x about
+chmod +x auto-reboot
+chmod +x restart
+chmod +x bw
+
+chmod +x port-ssl
+chmod +x port-ovpn
+
+chmod +x xp
+chmod +x acs-set
+chmod +x sshws
+cd
+
+
+cat > /etc/cron.d/re_otm <<-END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+0 2 * * * root /sbin/reboot
+END
+
+
+echo "50 * * * * root userdelexpired" >> /etc/crontab
 
 #hapus file
 cd
