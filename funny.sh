4421dd18f7ed80db1afdf497025449a9bc032bd2
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-11-19 20:24:07 +0700

    b

--- funny.sh
+++ funny.sh
@@ -30,6 +30,7 @@ mkdir /etc/xray
 mkdir /etc/nur
 #send
 echo $domain >> /etc/xray/domain
+echo $domain >> /etc/v2ray/domain
 echo $nsdomain >> /etc/xray/nsdomain
 echo $ipku >> /etc/xray/public
 echo $resdomain >> /etc/xray/resdomain
@@ -1038,6 +1039,40 @@ END
 
 
 echo "50 * * * * root userdelexpired" >> /etc/crontab
+
+
+cd
+echo "------------------------------------------------------------"
+echo ""
+echo ""
+echo "   >>> Service & Port"  | tee -a log-install.txt
+echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
+echo "   - SSH Websocket           : 80 " | tee -a log-install.txt
+echo "   - SSH SSL Websocket       : 443 " | tee -a log-install.txt
+echo "   - Stunnel4                : 447, 777" | tee -a log-install.txt
+echo "   - Dropbear                : 109, 143" | tee -a log-install.txt
+echo "   - Badvpn                  : 7100-7900" | tee -a log-install.txt
+echo "   - Nginx                   : 81" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
+echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
+echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
+echo "   - IPtables                : [ON]"  | tee -a log-install.txt
+echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
+echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
+echo "   - Autoreboot On           : $aureb:00 $gg GMT +7" | tee -a log-install.txt
+echo "   - Autobackup Data" | tee -a log-install.txt
+echo "   - AutoKill Multi Login User" | tee -a log-install.txt
+echo "   - Auto Delete Expired Account" | tee -a log-install.txt
+echo "   - Fully automatic script" | tee -a log-install.txt
+echo "   - VPS settings" | tee -a log-install.txt
+echo "   - Admin Control" | tee -a log-install.txt
+echo "   - Change port" | tee -a log-install.txt
+echo "   - Restore Data" | tee -a log-install.txt
+echo "   - Full Orders For Various Services" | tee -a log-install.txt
+echo ""
+clear
 
 #hapus file
 cd
