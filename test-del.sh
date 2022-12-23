483047a3d0814b56d585e99d6c0e2b89e5f1c2c2
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-12-23 23:33:26 +0700

    a

--- test-del.sh
+++ test-del.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+data=( `cat /etc/passwd | grep home | cut -d ' ' -f 1 | cut -d : -f 1`)
+touch kelazz
+mulog=$(mesinssh)
+
 #########################
 clear
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
@@ -10,12 +14,20 @@ echo ""
 read -p "Username SSH to Delete : " Pengguna
 
 if getent passwd $Pengguna > /dev/null 2>&1; then
-        killall -u $Pengguna > /dev/null 2>&1
         userdel $Pengguna > /dev/null 2>&1
         userdel -f -r $Pengguna > /dev/null 2>&1
         echo -e "User $Pengguna was removed."
+        sleep 1
 else
         echo -e "Failure: User $Pengguna Not Exist."
 fi
 
+echo -e "$mulog | grep $PID | cut -d ' ' -f 1" >> kelazz
+mia=( `cat kelazz`);
+for ehh in ${mia[@]}
+do
+kill $ehh
+done
+echo "sukses"
+rm kelazz
 clear
